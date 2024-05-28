8496375b0fb267f8ae61dd078a04835897130c9a
wnbaum <wnbaum04@gmail.com>
2024-05-28 16:28:52 -0400

    Fix node block canvas

--- addons/block_code/ui/block_canvas/node_block_canvas/node_block_canvas.gd
+++ addons/block_code/ui/block_canvas/node_block_canvas/node_block_canvas.gd
@@ -2,12 +2,6 @@
 class_name NodeBlockCanvas
 extends BlockCanvas
 
-var ready_block: EntryBlock
-var process_block: EntryBlock
-
 
 func _ready() -> void:
-	ready_block = (
-		preload("res://addons/block_code/ui/blocks/entry_block/entry_block.tscn").instantiate()
-	)
-	ready_block.drag_started.connect(_block_picked)
+	pass
