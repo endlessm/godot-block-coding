0a644ec30ed90d59f1c535c6cb0aa0f51cc19af2
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-06-10 17:50:47 -0300

    Blocks UI: Improve knobs in statement blocks

--- addons/block_code/ui/blocks/statement_block/background.gd
+++ addons/block_code/ui/blocks/statement_block/background.gd
@@ -1,9 +1,9 @@
 @tool
 extends Control
 
-const KNOB_X = 30.0
-const KNOB_W = 10.0
-const KNOB_H = 10.0
+const KNOB_X = 10.0
+const KNOB_W = 20.0
+const KNOB_H = 5.0
 const KNOB_Z = 5.0
 
 @export var color: Color:
@@ -15,13 +15,6 @@ func _set_color(new_color):
 	queue_redraw()
 
 
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-#print(color)
-#queue_redraw()
-#
-
-
 func float_array_to_Vector2Array(coords: Array) -> PackedVector2Array:
 	# Convert the array of floats into a PackedVector2Array.
 	var array: PackedVector2Array = []
@@ -31,18 +24,17 @@ func float_array_to_Vector2Array(coords: Array) -> PackedVector2Array:
 
 
 func _draw():
-	# draw_line(Vector2(size.x, 0), size, Color.GREEN, 5.0)
 	var polygon = [
 		[0.0, 0.0],
 		[KNOB_X, 0.0],
-		[KNOB_X - KNOB_Z, -KNOB_H],
-		[KNOB_X + KNOB_W, -KNOB_H],
-		[KNOB_X + KNOB_W - KNOB_Z, 0.0],
+		[KNOB_X + KNOB_Z, KNOB_H],
+		[KNOB_X + KNOB_Z + KNOB_W, KNOB_H],
+		[KNOB_X + KNOB_Z * 2 + KNOB_W, 0.0],
 		[size.x, 0.0],
 		[size.x, size.y],
-		[KNOB_X + KNOB_W - KNOB_Z, size.y],
-		[KNOB_X + KNOB_W, size.y - KNOB_H],
-		[KNOB_X - KNOB_Z, size.y - KNOB_H],
+		[KNOB_X + KNOB_Z * 2 + KNOB_W, size.y],
+		[KNOB_X + KNOB_Z + KNOB_W, size.y + KNOB_H],
+		[KNOB_X + KNOB_Z, size.y + KNOB_H],
 		[KNOB_X, size.y],
 		[0.0, size.y],
 		[0.0, 0.0],
