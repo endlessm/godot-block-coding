bc1ea20bcd83ab61ba7fafbf7324d7987d26f8da
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-06-12 07:41:05 -0300

    Blocks UI: Move knob constants to its own module

--- addons/block_code/ui/blocks/utilities/background/background.gd
+++ addons/block_code/ui/blocks/utilities/background/background.gd
@@ -1,10 +1,7 @@
 @tool
 extends Control
 
-const KNOB_X = 10.0
-const KNOB_W = 20.0
-const KNOB_H = 5.0
-const KNOB_Z = 5.0
+const Constants = preload("res://addons/block_code/ui/constants.gd")
 
 @export var color: Color:
 	set = _set_color
@@ -50,16 +47,16 @@ func float_array_to_Vector2Array(coords: Array) -> PackedVector2Array:
 func _draw():
 	var polygon = [
 		[0.0, 0.0],
-		[KNOB_X + shift_top, 0.0],
-		[KNOB_X + KNOB_Z + shift_top, KNOB_H],
-		[KNOB_X + KNOB_Z + KNOB_W + shift_top, KNOB_H],
-		[KNOB_X + KNOB_Z * 2 + KNOB_W + shift_top, 0.0],
+		[Constants.KNOB_X + shift_top, 0.0],
+		[Constants.KNOB_X + Constants.KNOB_Z + shift_top, Constants.KNOB_H],
+		[Constants.KNOB_X + Constants.KNOB_Z + Constants.KNOB_W + shift_top, Constants.KNOB_H],
+		[Constants.KNOB_X + Constants.KNOB_Z * 2 + Constants.KNOB_W + shift_top, 0.0],
 		[size.x, 0.0],
 		[size.x, size.y],
-		[KNOB_X + KNOB_Z * 2 + KNOB_W + shift_bottom, size.y],
-		[KNOB_X + KNOB_Z + KNOB_W + shift_bottom, size.y + KNOB_H],
-		[KNOB_X + KNOB_Z + shift_bottom, size.y + KNOB_H],
-		[KNOB_X + shift_bottom, size.y],
+		[Constants.KNOB_X + Constants.KNOB_Z * 2 + Constants.KNOB_W + shift_bottom, size.y],
+		[Constants.KNOB_X + Constants.KNOB_Z + Constants.KNOB_W + shift_bottom, size.y + Constants.KNOB_H],
+		[Constants.KNOB_X + Constants.KNOB_Z + shift_bottom, size.y + Constants.KNOB_H],
+		[Constants.KNOB_X + shift_bottom, size.y],
 		[0.0, size.y],
 		[0.0, 0.0],
 	]
