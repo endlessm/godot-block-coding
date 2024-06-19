859e293664210b9142d4c6f0d6dcef00ebd3ba25
Dan Nicholson <dbn@endlessos.org>
2024-06-19 16:23:48 -0600

    simple_character: Move custom blocks to Input section
    While these do have to do with movement, they happen because of input
    events.
    
    https://phabricator.endlessm.com/T35507

--- addons/block_code/simple_nodes/simple_character/simple_character.gd
+++ addons/block_code/simple_nodes/simple_character/simple_character.gd
@@ -17,10 +17,6 @@ static func get_exposed_properties() -> Array[String]:
 	return ["position"]
 
 
-static func get_custom_categories() -> Array[BlockCategory]:
-	return [BlockCategory.new("Movement", Color("4a86d5"))]
-
-
 static func get_custom_blocks() -> Array[Block]:
 	var b: Block
 	var block_list: Array[Block] = []
@@ -30,14 +26,14 @@ static func get_custom_blocks() -> Array[Block]:
 	b.block_type = Types.BlockType.EXECUTE
 	b.block_format = "Move with player 1 buttons, speed {speed: INT}"
 	b.statement = 'velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")*{speed}\n' + "move_and_slide()"
-	b.category = "Movement"
+	b.category = "Input"
 	block_list.append(b)
 
 	b = CategoryFactory.BLOCKS["statement_block"].instantiate()
 	b.block_type = Types.BlockType.EXECUTE
 	b.block_format = "Move with player 2 buttons, speed {speed: INT}"
 	b.statement = 'velocity = Input.get_vector("player_2_left", "player_2_right", "player_2_up", "player_2_down")*{speed}\n' + "move_and_slide()"
-	b.category = "Movement"
+	b.category = "Input"
 	block_list.append(b)
 
 	return block_list
