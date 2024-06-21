154c705d0de75bba3318c1c1e44da4f425fe0cba
Dylan McCall <dylan@dylanmccall.ca>
2024-06-21 15:47:40 -0700

    Don't vertically expand block snap previews
    https://phabricator.endlessm.com/T35519

--- addons/block_code/drag_manager/drag_manager.gd
+++ addons/block_code/drag_manager/drag_manager.gd
@@ -89,6 +89,7 @@ func _update_preview(snap_point: SnapPoint):
 		preview_block.color = Color(1, 1, 1, 0.5)
 		preview_block.custom_minimum_size = dragging.get_global_rect().size
 		preview_block.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
+		preview_block.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
 
 		previewing_snap_point.add_child(preview_block)
 
