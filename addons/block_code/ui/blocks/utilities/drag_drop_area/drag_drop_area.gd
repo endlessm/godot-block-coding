af807afca8e7dbac686e89a0441447cfee6b35e8
Dylan McCall <dylan@dylanmccall.ca>
2024-06-24 17:20:49 -0700

    Reset block size when snap points are changed
    Previously, blocks were continuing to occupy space after their children
    were detached, making it difficult to interact with other blocks in the
    canvas.

--- addons/block_code/ui/blocks/utilities/drag_drop_area/drag_drop_area.gd
+++ addons/block_code/ui/blocks/utilities/drag_drop_area/drag_drop_area.gd
@@ -5,22 +5,13 @@ extends MarginContainer
 signal mouse_down
 signal mouse_up
 
-var hovered := false
-
-
-func _on_mouse_entered():
-	hovered = true
-
-
-func _on_mouse_exited():
-	hovered = false
-
 
 func _on_gui_input(event):
-	if hovered:
-		if event is InputEventMouseButton:
-			var mouse_event: InputEventMouseButton = event as InputEventMouseButton
-			if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
-				mouse_down.emit()
-			if mouse_event.button_index == MOUSE_BUTTON_LEFT and not mouse_event.pressed:
-				mouse_up.emit()
+	if event is InputEventMouseButton and get_global_rect().has_point(event.global_position):
+		var mouse_event: InputEventMouseButton = event as InputEventMouseButton
+		if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
+			mouse_down.emit()
+			get_viewport().set_input_as_handled()
+		if mouse_event.button_index == MOUSE_BUTTON_LEFT and not mouse_event.pressed:
+			mouse_up.emit()
+			get_viewport().set_input_as_handled()
