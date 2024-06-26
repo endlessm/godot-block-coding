b9bd33035fd4e52cc5566d90e94ce4e3b29a4ebb
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-06-25 23:27:44 -0300

    Convert scoring logic to blocks
    The SimpleScoring scene exposes 4 blocks for setting / changing the
    player 1 (left) and player 2 (right) scores.
    
    Remove the custom blocks previously in the root node.
    
    Also, update the blocks in the demo.
    
    https://phabricator.endlessm.com/T35516

--- addons/block_code/simple_nodes/simple_scoring/simple_scoring.gd
+++ addons/block_code/simple_nodes/simple_scoring/simple_scoring.gd
@@ -1,9 +1,46 @@
+@tool
+class_name SimpleScoring
 extends CanvasLayer
+
+@export var score_left: int:
+	set = _set_score_left
+
+@export var score_right: int:
+	set = _set_score_right
 
 @onready var _score_labels = {
 	"left": %PlayerLeftScore,
 	"right": %PlayerRightScore,
 }
+
+const _POSITIONS_FOR_PLAYER = {
+	"1": "left",
+	"2": "right",
+}
+
+
+func get_custom_class():
+	return "SimpleScoring"
+
+
+func _set_score_left(new_score_left):
+	score_left = new_score_left
+	if not is_node_ready():
+		await ready
+	if score_left:
+		_update_label("left", score_left)
+
+
+func _set_score_right(new_score_right):
+	score_right = new_score_right
+	if not is_node_ready():
+		await ready
+	if score_right:
+		_update_label("right", score_right)
+
+
+func _update_label(player, score):
+	_score_labels[player].text = str(score)
 
 
 ## Sets the score for one player.
@@ -13,7 +50,23 @@ func set_player_score(player: String, score: int):
 		_score_labels[player].text = str(score)
 
 
-## Sets the score for each player.
-func set_players_scores(left_score: int, right_score: int):
-	set_player_score("left", left_score)
-	set_player_score("right", right_score)
+static func get_custom_blocks() -> Array[Block]:
+	var b: Block
+	var block_list: Array[Block] = []
+
+	for player in _POSITIONS_FOR_PLAYER:
+		b = CategoryFactory.BLOCKS["statement_block"].instantiate()
+		b.block_type = Types.BlockType.EXECUTE
+		b.block_format = "Set player %s score to {score: INT}" % player
+		b.statement = "score_%s = {score}" % _POSITIONS_FOR_PLAYER[player]
+		b.category = "Scoring"
+		block_list.append(b)
+
+		b = CategoryFactory.BLOCKS["statement_block"].instantiate()
+		b.block_type = Types.BlockType.EXECUTE
+		b.block_format = "Change player %s score by {score: INT}" % player
+		b.statement = "score_%s += {score}" % _POSITIONS_FOR_PLAYER[player]
+		b.category = "Scoring"
+		block_list.append(b)
+
+	return block_list
