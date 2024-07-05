1fdfe85f577a35d1710db5ba96c289895577181f
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-07-05 15:49:21 -0300

    SimpleCharacter: Use methods for player movement
    Add 2 methods and call them, instead of having the implementation in the
    code template ("statement" property).
    
    This improves a bit when serializing the implementation. But most
    importantly, allows the 2 blocks to be added to the same node without a
    collision on the var dir being declared twice.

--- addons/block_code/simple_nodes/simple_character/simple_character.gd
+++ addons/block_code/simple_nodes/simple_character/simple_character.gd
@@ -5,6 +5,23 @@ extends CharacterBody2D
 @export var texture: Texture2D:
 	set = _set_texture
 
+const PLAYER_KEYS = {
+	"player_1":
+	{
+		"up": KEY_W,
+		"down": KEY_S,
+		"left": KEY_A,
+		"right": KEY_D,
+	},
+	"player_2":
+	{
+		"up": KEY_UP,
+		"down": KEY_DOWN,
+		"left": KEY_LEFT,
+		"right": KEY_RIGHT,
+	}
+}
+
 
 func _set_texture(new_texture):
 	texture = new_texture
@@ -30,6 +47,16 @@ func get_custom_class():
 	return "SimpleCharacter"
 
 
+func move_with_player_buttons(player: String, speed: Vector2):
+	var dir = Vector2()
+	dir.x += float(Input.is_physical_key_pressed(PLAYER_KEYS[player]["right"]))
+	dir.x -= float(Input.is_physical_key_pressed(PLAYER_KEYS[player]["left"]))
+	dir.y += float(Input.is_physical_key_pressed(PLAYER_KEYS[player]["down"]))
+	dir.y -= float(Input.is_physical_key_pressed(PLAYER_KEYS[player]["up"]))
+	velocity = dir.normalized() * speed
+	move_and_slide()
+
+
 static func get_custom_blocks() -> Array[Block]:
 	var b: Block
 	var block_list: Array[Block] = []
@@ -38,32 +65,14 @@ static func get_custom_blocks() -> Array[Block]:
 	b = CategoryFactory.BLOCKS["statement_block"].instantiate()
 	b.block_type = Types.BlockType.EXECUTE
 	b.block_format = "Move with player 1 buttons, speed {speed: VECTOR2}"
-	b.statement = (
-		"var dir = Vector2()\n"
-		+ "dir.x += float(Input.is_physical_key_pressed(KEY_D))\n"
-		+ "dir.x -= float(Input.is_physical_key_pressed(KEY_A))\n"
-		+ "dir.y += float(Input.is_physical_key_pressed(KEY_S))\n"
-		+ "dir.y -= float(Input.is_physical_key_pressed(KEY_W))\n"
-		+ "dir = dir.normalized()\n"
-		+ "velocity = dir*{speed}\n"
-		+ "move_and_slide()"
-	)
+	b.statement = "move_with_player_buttons('player_1', {speed})"
 	b.category = "Input"
 	block_list.append(b)
 
 	b = CategoryFactory.BLOCKS["statement_block"].instantiate()
 	b.block_type = Types.BlockType.EXECUTE
 	b.block_format = "Move with player 2 buttons, speed {speed: VECTOR2}"
-	b.statement = (
-		"var dir = Vector2()\n"
-		+ "dir.x += float(Input.is_physical_key_pressed(KEY_RIGHT))\n"
-		+ "dir.x -= float(Input.is_physical_key_pressed(KEY_LEFT))\n"
-		+ "dir.y += float(Input.is_physical_key_pressed(KEY_DOWN))\n"
-		+ "dir.y -= float(Input.is_physical_key_pressed(KEY_UP))\n"
-		+ "dir = dir.normalized()\n"
-		+ "velocity = dir*{speed}\n"
-		+ "move_and_slide()"
-	)
+	b.statement = "move_with_player_buttons('player_2', {speed})"
 	b.category = "Input"
 	block_list.append(b)
 
