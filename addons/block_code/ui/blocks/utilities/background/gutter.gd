854330dc97bd07b70a4d785eebbb0613d11ec967
Dylan McCall <dylan@dylanmccall.ca>
2024-07-31 18:40:38 -0700

    Change Background border color when block is focused

--- addons/block_code/ui/blocks/utilities/background/gutter.gd
+++ addons/block_code/ui/blocks/utilities/background/gutter.gd
@@ -1,9 +1,11 @@
 @tool
 extends Control
 
+const BlockTreeUtil = preload("res://addons/block_code/ui/block_tree_util.gd")
 const Constants = preload("res://addons/block_code/ui/constants.gd")
 
 var outline_color: Color
+var parent_block: Block
 
 @export var color: Color:
 	set = _set_color
@@ -13,6 +15,12 @@ func _set_color(new_color):
 	color = new_color
 	outline_color = color.darkened(0.2)
 	queue_redraw()
+
+
+func _ready():
+	parent_block = BlockTreeUtil.get_parent_block(self)
+	parent_block.focus_entered.connect(queue_redraw)
+	parent_block.focus_exited.connect(queue_redraw)
 
 
 func _draw():
@@ -33,5 +41,5 @@ func _draw():
 	right_polygon.append(Vector2(size.x, size.y))
 
 	draw_colored_polygon(fill_polygon, color)
-	draw_polyline(left_polygon, outline_color, Constants.OUTLINE_WIDTH)
-	draw_polyline(right_polygon, outline_color, Constants.OUTLINE_WIDTH)
+	draw_polyline(left_polygon, Constants.FOCUS_BORDER_COLOR if parent_block.has_focus() else outline_color, Constants.OUTLINE_WIDTH)
+	draw_polyline(right_polygon, Constants.FOCUS_BORDER_COLOR if parent_block.has_focus() else outline_color, Constants.OUTLINE_WIDTH)
