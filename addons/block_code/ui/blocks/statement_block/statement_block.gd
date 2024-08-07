79d9bea703b3e25fb45425caf73cffe7353ca637
Will Baumgartner <wnb@endlessos.org>
2024-08-07 17:10:22 -0400

    Refactor UI block classes to depend on BlockDefinition

--- addons/block_code/ui/blocks/statement_block/statement_block.gd
+++ addons/block_code/ui/blocks/statement_block/statement_block.gd
@@ -5,46 +5,33 @@ extends Block
 const ParameterInput = preload("res://addons/block_code/ui/blocks/utilities/parameter_input/parameter_input.gd")
 const ParameterInputScene = preload("res://addons/block_code/ui/blocks/utilities/parameter_input/parameter_input.tscn")
 
-@export var block_format: String = ""
-@export var statement: String = ""
-@export var defaults: Dictionary = {}
-
 @onready var _background := %Background
 @onready var _hbox := %HBoxContainer
 
-var param_name_input_pairs: Array
-var param_input_strings: Dictionary  # Only loaded from serialized
+var arg_name_to_param_input_dict: Dictionary
+var args_to_add_after_format: Dictionary  # Only used when loading
 
 
 func _ready():
 	super()
 
-	if block_type != Types.BlockType.STATEMENT:
+	if definition.type != Types.BlockType.STATEMENT:
 		_background.show_top = false
 	_background.color = color
 
 	format()
 
-	if param_input_strings:
-		for pair in param_name_input_pairs:
-			pair[1].set_raw_input(param_input_strings[pair[0]])
+	for arg_name in arg_name_to_param_input_dict:
+		if arg_name in args_to_add_after_format:
+			var argument = args_to_add_after_format[arg_name]
+			if argument is Block:
+				arg_name_to_param_input_dict[arg_name].snap_point.add_child(argument)
+			else:
+				arg_name_to_param_input_dict[arg_name].set_raw_input(argument)
 
 
 func _on_drag_drop_area_mouse_down():
 	_drag_started()
-
-
-func get_serialized_props() -> Array:
-	var props := super()
-	if not BlocksCatalog.has_block(block_name):
-		props.append_array(serialize_props(["block_format", "statement", "defaults"]))
-
-	var _param_input_strings: Dictionary = {}
-	for pair in param_name_input_pairs:
-		_param_input_strings[pair[0]] = pair[1].get_raw_input()
-
-	props.append(["param_input_strings", _param_input_strings])
-	return props
 
 
 static func get_block_class():
@@ -56,11 +43,11 @@ static func get_scene_path():
 
 
 func format():
-	param_name_input_pairs = format_string(self, %HBoxContainer, block_format, defaults)
+	arg_name_to_param_input_dict = format_string(self, %HBoxContainer, definition.display_template, definition.defaults)
 
 
-static func format_string(parent_block: Block, attach_to: Node, string: String, _defaults: Dictionary) -> Array:
-	var _param_name_input_pairs = []
+static func format_string(parent_block: Block, attach_to: Node, string: String, _defaults: Dictionary) -> Dictionary:
+	var _arg_name_to_param_input_dict = {}
 	var regex = RegEx.new()
 	regex.compile("\\[([^\\]]+)\\]|\\{([^}]+)\\}")  # Capture things of format {test} or [test]
 	var results := regex.search_all(string)
@@ -98,13 +85,18 @@ static func format_string(parent_block: Block, attach_to: Node, string: String, 
 		if copy_block:
 			var parameter_output: ParameterOutput = preload("res://addons/block_code/ui/blocks/utilities/parameter_output/parameter_output.tscn").instantiate()
 			parameter_output.name = "ParameterOutput%d" % start  # Unique path
-			parameter_output.block_params = {
-				"block_format": param_name,
-				"statement": param_name,
-				"variant_type": param_type,
-				"color": parent_block.color,
-				"scope": parent_block.get_entry_statement() if parent_block is EntryBlock else ""
-			}
+
+			# FIXME: This logic could be separated because it is the same as in CategoryFactory get_parameter_output_blocks()
+			var block_definition = BlockDefinition.new()
+			block_definition.name = parent_block.definition.name + "_" + param_name
+			block_definition.category = parent_block.definition.category
+			block_definition.type = Types.BlockType.VALUE
+			block_definition.variant_type = param_type
+			block_definition.display_template = param_name
+			block_definition.code_template = param_name
+			block_definition.scope = parent_block.definition.code_template
+
+			parameter_output.block_params = {"definition": block_definition, "color": parent_block.color}
 			parameter_output.block = parent_block
 			attach_to.add_child(parameter_output)
 		else:
@@ -118,10 +110,11 @@ static func format_string(parent_block: Block, attach_to: Node, string: String, 
 			parameter_input.modified.connect(func(): parent_block.modified.emit())
 
 			attach_to.add_child(parameter_input)
-			if param_default:
+
+			if param_default != null:
 				parameter_input.set_raw_input(param_default)
 
-			_param_name_input_pairs.append([param_name, parameter_input])
+			_arg_name_to_param_input_dict[param_name] = parameter_input
 
 		start = result.get_end()
 
@@ -132,4 +125,4 @@ static func format_string(parent_block: Block, attach_to: Node, string: String, 
 		label.text = label_text
 		attach_to.add_child(label)
 
-	return _param_name_input_pairs
+	return _arg_name_to_param_input_dict
