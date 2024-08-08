f5ce15e3ac7c9492bcbf8615cfbc16425e9f4fa1
Will Baumgartner <wnb@endlessos.org>
2024-08-08 16:40:02 -0400

    Modify BlockDefinition resource
    Export variables and refactor properties
    
    Make it a tool script so that this resource can execute code in the editor (will be useful for overloading etc. in the future)

--- addons/block_code/code_generation/block_definition.gd
+++ addons/block_code/code_generation/block_definition.gd
@@ -1,21 +1,47 @@
+@tool
+
 extends Resource
 
 const Types = preload("res://addons/block_code/types/types.gd")
 
-var name: StringName
-var type: Types.BlockType
-var description: String
-var category: String
+@export var name: StringName
+@export var description: String
+@export var category: String
 
-var label_template: String
-var code_template: String
-var defaults: Dictionary = {}
+@export var type: Types.BlockType
+@export var variant_type: Variant.Type
+
+@export var display_template: String
+@export var code_template: String
+@export var defaults: Dictionary
 
 ## Only for blocks of type Types.ENTRY. If non-empty, this block defines a
 ## callback that will be connected to the signal with this name.
-var signal_name: String = ""
+@export var signal_name: String
+
+## Empty except for blocks that have a defined scope
+@export var scope: String
 
 
-func _init(p_name: StringName, p_type: Types.BlockType):
+func _init(
+	p_name: StringName = &"",
+	p_description: String = "",
+	p_category: String = "",
+	p_type: Types.BlockType = Types.BlockType.STATEMENT,
+	p_variant_type: Variant.Type = TYPE_NIL,
+	p_display_template: String = "",
+	p_code_template: String = "",
+	p_defaults = {},
+	p_signal_name: String = "",
+	p_scope: String = "",
+):
 	name = p_name
+	description = p_description
+	category = p_category
 	type = p_type
+	variant_type = p_variant_type
+	display_template = p_display_template
+	code_template = p_code_template
+	defaults = p_defaults
+	signal_name = p_signal_name
+	scope = p_scope
