724969706dc048b1a169eee1497e86ab2fae344a
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-08-12 16:56:30 -0300

    Blocks catalog: Read definitions from resource files
    And remove the 2 definitions from code.
    
    Please note the name of the On Ready block has changed. This will be
    addressed in the next commit.

--- addons/block_code/code_generation/blocks_catalog.gd
+++ addons/block_code/code_generation/blocks_catalog.gd
@@ -2,6 +2,9 @@ extends Object
 
 const BlockDefinition = preload("res://addons/block_code/code_generation/block_definition.gd")
 const Types = preload("res://addons/block_code/types/types.gd")
+const Util = preload("res://addons/block_code/code_generation/util.gd")
+
+const _BLOCKS_PATH = "res://addons/block_code/blocks/"
 
 static var _catalog: Dictionary
 
@@ -11,24 +14,11 @@ static func setup():
 		return
 
 	_catalog = {}
-	var block_definition: BlockDefinition = BlockDefinition.new()
-	block_definition.name = &"ready_block"
-	block_definition.type = Types.BlockType.ENTRY
-	block_definition.display_template = "On Ready"
-	block_definition.code_template = "func _ready():"
-	block_definition.description = 'Attached blocks will be executed once when the node is "ready"'
-	block_definition.category = "Lifecycle"
-	_catalog[&"ready_block"] = block_definition
 
-	block_definition = BlockDefinition.new()
-	block_definition.name = &"print"
-	block_definition.type = Types.BlockType.STATEMENT
-	block_definition.display_template = "print {text: STRING}"
-	block_definition.code_template = "print({text})"
-	block_definition.defaults = {"text": "Hello"}
-	block_definition.description = "Print the text to output"
-	block_definition.category = "Log"
-	_catalog[&"print"] = block_definition
+	var definition_files = Util.get_files_in_dir_recursive(_BLOCKS_PATH, "*.tres")
+	for file in definition_files:
+		var block_definition = load(file)
+		_catalog[block_definition.name] = block_definition
 
 
 static func get_block(block_name: StringName):
