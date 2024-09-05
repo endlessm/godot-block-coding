b79aef5f09463ccfc2941ee7c07fb68fd73eeb20
Dan Nicholson <dbn@endlessos.org>
2024-09-05 15:51:44 -0600

    serialization: Add methods for getting script block definitions
    These will be used to populate the picker rather than doing it in the UI
    layer.

--- addons/block_code/serialization/block_script_serialization.gd
+++ addons/block_code/serialization/block_script_serialization.gd
@@ -1,6 +1,10 @@
+@tool
 class_name BlockScriptSerialization
 extends Resource
 
+const BlocksCatalog = preload("res://addons/block_code/code_generation/blocks_catalog.gd")
+const BlockCategory = preload("res://addons/block_code/ui/picker/categories/block_category.gd")
+const BlockDefinition = preload("res://addons/block_code/code_generation/block_definition.gd")
 const BlockSerializationTree = preload("res://addons/block_code/serialization/block_serialization_tree.gd")
 const VariableDefinition = preload("res://addons/block_code/code_generation/variable_definition.gd")
 
@@ -19,3 +23,24 @@ func _init(
 	generated_script = p_generated_script
 	variables = p_variables
 	version = p_version
+
+
+func get_definitions() -> Array[BlockDefinition]:
+	for class_dict in ProjectSettings.get_global_class_list():
+		if class_dict.class == script_inherits:
+			var script = load(class_dict.path)
+			if script.has_method("setup_custom_blocks"):
+				script.setup_custom_blocks()
+			break
+
+	return BlocksCatalog.get_inherited_blocks(script_inherits)
+
+
+func get_categories() -> Array[BlockCategory]:
+	for class_dict in ProjectSettings.get_global_class_list():
+		if class_dict.class == script_inherits:
+			var script = load(class_dict.path)
+			if script.has_method("get_custom_categories"):
+				return script.get_custom_categories()
+
+	return []
