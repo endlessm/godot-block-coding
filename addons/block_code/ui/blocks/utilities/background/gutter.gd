73ba94c7da7512ef47cfbd5e8aac66f2187fa2e3
Daze <pating.wolfe@yahoo.com>
2024-10-14 14:06:48 +0000

    Background shapes, optional bottom tab, optional outlines
    Adds 8 new shapes for the background used in blocks, the bottom tab can be hidden and is shown by default, makes outlines optional and are shown by default

--- addons/block_code/ui/blocks/utilities/background/gutter.gd
+++ addons/block_code/ui/blocks/utilities/background/gutter.gd
@@ -10,10 +10,19 @@ var parent_block: Block
 @export var color: Color:
 	set = _set_color
 
+## Variant
+@export var variant: bool = 0:
+	set = _variant
+
 
 func _set_color(new_color):
 	color = new_color
 	outline_color = color.darkened(0.2)
+	queue_redraw()
+
+
+func _variant(new_variant):
+	variant = new_variant
 	queue_redraw()
 
 
@@ -25,20 +34,20 @@ func _ready():
 
 func _draw():
 	var fill_polygon: PackedVector2Array
-	fill_polygon.append(Vector2(0.0, 0.0))
-	fill_polygon.append(Vector2(size.x, 0.0))
-	fill_polygon.append(Vector2(size.x, size.y))
-	fill_polygon.append(Vector2(0.0, size.y))
-	fill_polygon.append(Vector2(0.0, 0.0))
+	fill_polygon.append(Vector2(5.0 if variant else 0.0, 0.0))
+	fill_polygon.append(Vector2(size.x + (5.0 if variant else 0.0), 0.0))
+	fill_polygon.append(Vector2(size.x + (5.0 if variant else 0.0), size.y))
+	fill_polygon.append(Vector2(5.0 if variant else 0.0, size.y))
+	fill_polygon.append(Vector2(5.0 if variant else 0.0, 0.0))
 
 	var left_polygon: PackedVector2Array
 	var right_polygon: PackedVector2Array
 
-	left_polygon.append(Vector2(0.0, 0.0))
-	left_polygon.append(Vector2(0.0, size.y))
+	left_polygon.append(Vector2(5.0 if variant else 0.0, 0.0))
+	left_polygon.append(Vector2(5.0 if variant else 0.0, size.y))
 
-	right_polygon.append(Vector2(size.x, 0.0))
-	right_polygon.append(Vector2(size.x, size.y))
+	right_polygon.append(Vector2(size.x + (5.0 if variant else 0.0), 0.0))
+	right_polygon.append(Vector2(size.x + (5.0 if variant else 0.0), size.y))
 
 	draw_colored_polygon(fill_polygon, color)
 	draw_polyline(left_polygon, Constants.FOCUS_BORDER_COLOR if parent_block.has_focus() else outline_color, Constants.OUTLINE_WIDTH)
