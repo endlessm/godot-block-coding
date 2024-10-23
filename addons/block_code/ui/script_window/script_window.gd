527bc4b6f7b3090753e2b132ddb1503c9b6c3d32
Will Thompson <wjt@endlessos.org>
2024-10-23 16:15:14 +0100

    Script Window: Attempt to match editor's syntax highlighting
    If there is at least one script open in the editor, we can steal its syntax
    highlighter and apply it to the script window's CodeEdit control.
    
    Otherwise, we continue to use a built-in SyntaxHighlighter.

--- addons/block_code/ui/script_window/script_window.gd
+++ addons/block_code/ui/script_window/script_window.gd
@@ -6,7 +6,20 @@ var script_content: String = ""
 @onready var script_label: CodeEdit = %Code
 
 
+## Attempts to match the syntax highlighting for some open script in the
+## editor, which is more likely to be appropriate for the editor theme
+## than our hardcoded default
+func _apply_editor_syntax_highlighter():
+	var script_editor: ScriptEditor = EditorInterface.get_script_editor()
+	for x in script_editor.get_open_script_editors():
+		var control: Control = x.get_base_editor()
+		if control is TextEdit:
+			script_label.syntax_highlighter = control.syntax_highlighter
+			break
+
+
 func _ready():
+	_apply_editor_syntax_highlighter()
 	script_label.text = script_content.replace("\t", "    ")
 
 
