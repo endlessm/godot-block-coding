78f77759fefb69bfa2a9954b878f2eb616b70223
Dan Nicholson <dbn@endlessos.org>
2024-10-24 12:24:38 -0600

    serialization: Handle renamed blocks
    If a block has been renamed, loading a scene with the old block name
    will fail and the user will be required to replace the block. Handle
    renamed blocks by setting the name to the new value in the serialization
    resources. In order for the setter to run in the editor, the classes
    need to be tool scripts.
    
    https://phabricator.endlessm.com/T35547

--- addons/block_code/serialization/block_serialization.gd
+++ addons/block_code/serialization/block_serialization.gd
@@ -1,8 +1,10 @@
+@tool
 extends Resource
 
 const BlockSerialization = preload("res://addons/block_code/serialization/block_serialization.gd")
 
-@export var name: StringName
+@export var name: StringName:
+	set = _set_name
 @export var children: Array[BlockSerialization]
 @export var arguments: Dictionary  # String, ValueBlockSerialization
 
@@ -11,3 +13,18 @@ func _init(p_name: StringName = &"", p_children: Array[BlockSerialization] = [],
 	name = p_name
 	children = p_children
 	arguments = p_arguments
+
+
+# Block name backwards compatibility handling.
+const _renamed_blocks: Dictionary = {}
+
+
+func _set_name(value):
+	var new_name = _renamed_blocks.get(value)
+	if new_name:
+		print("Migrating block %s to new name %s" % [value, new_name])
+		name = new_name
+		if Engine.is_editor_hint():
+			EditorInterface.mark_scene_as_unsaved()
+	else:
+		name = value
