endlessm/godot-block-coding

commit 89beea94eefa26d47e5059f73a4804f771323fae

BlockCanvas: Implement drag & drop the node's property from Inspector
Dragging the node's property from Inspector shows the object's type as
"obj_property". So, allow the "obj_property" type in _can_drop_data()
and handle it in _drop_data(). Then, implement getting the property's
value as a Variable block with _drop_obj_property() in detail.

https://phabricator.endlessm.com/T35649

addons/block_code/ui/block_canvas/block_canvas.gd
@@ -4,9 +4,11 @@ extends MarginContainer
 const ASTList = preload("res://addons/block_code/code_generation/ast_list.gd")
 const BlockAST = preload("res://addons/block_code/code_generation/block_ast.gd")
 const BlockCodePlugin = preload("res://addons/block_code/block_code_plugin.gd")
+const BlockDefinition = preload("res://addons/block_code/code_generation/block_definition.gd")
 const BlockTreeUtil = preload("res://addons/block_code/ui/block_tree_util.gd")
 const DragManager = preload("res://addons/block_code/drag_manager/drag_manager.gd")
 const ScriptGenerator = preload("res://addons/block_code/code_generation/script_generator.gd")
+const Types = preload("res://addons/block_code/types/types.gd")
 const Util = preload("res://addons/block_code/ui/util.gd")
 
 const EXTEND_MARGIN: float = 800
@@ -75,6 +77,10 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	if typeof(data) != TYPE_DICTIONARY:
 		return false
 
+	# Allow dropping property block
+	if data.get("type", "") == "obj_property":
+		return true
+
 	var nodes: Array = data.get("nodes", [])
 	if nodes.size() != 1:
 		return false
@@ -94,6 +100,8 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 func _drop_data(at_position: Vector2, data: Variant) -> void:
 	if data["type"] == "nodes":
 		_drop_node(at_position, data)
+	elif data["type"] == "obj_property":
+		_drop_obj_property(at_position, data)
 
 
 func _drop_node(at_position: Vector2, data: Variant) -> void:
@@ -109,6 +117,30 @@ func _drop_node(at_position: Vector2, data: Variant) -> void:
 
 	var block = _context.block_script.instantiate_block_by_name(&"get_node")
 	block.set_parameter_values_on_ready({"path": node_path})
+	add_block(block, at_position)
+	reconnect_block.emit(block)
+
+
+func _drop_obj_property(at_position: Vector2, data: Variant) -> void:
+	var object_name = str(data["object"]).get_slice(":", 0)
+	var property_name = data["property"]
+	var property_value = data["value"]
+
+	# Prepare a block to get the property's value
+	var block_definition = (
+		BlockDefinition
+		. new(
+			&"%s_get_%s" % [object_name, property_name],
+			object_name,
+			"The %s property" % property_name,
+			"Variables",
+			Types.BlockType.VALUE,
+			typeof(property_value),
+			"%s" % property_name.capitalize().to_lower(),
+			"%s" % property_name,
+		)
+	)
+	var block = _context.block_script.instantiate_block(block_definition)
 	add_block(block, at_position)
 	reconnect_block.emit(block)
 

commit 98db5ef737d2bca49a06ef7684ef458e6c070fe1

BlockCanvas: Introduce get_property_(get|set)ter_block_definition for drop object property
The object property's getter & setter blocks generating code is similar
to get_variable_(get|set)ter_block_definition(). So, reuse them within
get_property_(get|set)ter_block_definition() to generate property's
blocks including the description.

And, the block name will change to: "(get|set)_var_<property name>".

Fixes: 89beea94eefa ("BlockCanvas: Implement drag & drop the node's property from Inspector")
https://phabricator.endlessm.com/T35649

addons/block_code/code_generation/blocks_catalog.gd
@@ -287,6 +287,18 @@ static func get_variable_setter_block_definition(variable: VariableDefinition) -
 	block_def.category = "Variables"
 	block_def.type = Types.BlockType.STATEMENT
 	block_def.display_template = "Set %s to {value: %s}" % [variable.var_name, type_string]
-	block_def.code_template = "%s = {value}" % [variable.var_name]
+	block_def.code_template = "%s = {value}" % variable.var_name
 
+	return block_def
+
+
+static func get_property_getter_block_definition(variable: VariableDefinition) -> BlockDefinition:
+	var block_def := get_variable_getter_block_definition(variable)
+	block_def.description = "The %s property" % variable.var_name
+	return block_def
+
+
+static func get_property_setter_block_definition(variable: VariableDefinition) -> BlockDefinition:
+	var block_def := get_variable_setter_block_definition(variable)
+	block_def.description = "Set the %s property" % variable.var_name
 	return block_def

addons/block_code/ui/block_canvas/block_canvas.gd
@@ -3,6 +3,7 @@ extends MarginContainer
 
 const ASTList = preload("res://addons/block_code/code_generation/ast_list.gd")
 const BlockAST = preload("res://addons/block_code/code_generation/block_ast.gd")
+const BlocksCatalog = preload("res://addons/block_code/code_generation/blocks_catalog.gd")
 const BlockCodePlugin = preload("res://addons/block_code/block_code_plugin.gd")
 const BlockDefinition = preload("res://addons/block_code/code_generation/block_definition.gd")
 const BlockTreeUtil = preload("res://addons/block_code/ui/block_tree_util.gd")
@@ -10,6 +11,7 @@ const DragManager = preload("res://addons/block_code/drag_manager/drag_manager.g
 const ScriptGenerator = preload("res://addons/block_code/code_generation/script_generator.gd")
 const Types = preload("res://addons/block_code/types/types.gd")
 const Util = preload("res://addons/block_code/ui/util.gd")
+const VariableDefinition = preload("res://addons/block_code/code_generation/variable_definition.gd")
 
 const EXTEND_MARGIN: float = 800
 const BLOCK_AUTO_PLACE_MARGIN: Vector2 = Vector2(25, 8)
@@ -124,44 +126,20 @@ func _drop_node(at_position: Vector2, data: Variant) -> void:
 
 
 func _drop_obj_property(at_position: Vector2, data: Variant) -> void:
-	var object_name = str(data["object"]).get_slice(":", 0)
 	var property_name = data["property"]
 	var property_value = data["value"]
+	var is_getter = !_modifier_ctrl
 
 	# Prepare a Variable block to set / get the property's value according to
 	# the modifier KEY_CTRL pressing.
+	var variable := VariableDefinition.new(property_name, typeof(property_value))
 	var block_definition: BlockDefinition
-	var property_type = typeof(property_value)
-	if _modifier_ctrl:
-		var type_string: String = Types.VARIANT_TYPE_TO_STRING[property_type]
-		block_definition = (
-			BlockDefinition
-			. new(
-				&"%s_set_%s" % [object_name, property_name],
-				object_name,
-				"Set the %s property" % property_name,
-				"Variables",
-				Types.BlockType.STATEMENT,
-				property_type,
-				"set %s to {value: %s}" % [property_name.capitalize().to_lower(), type_string],
-				"%s = {value}" % property_name,
-				{"value": property_value},
-			)
-		)
+
+	if is_getter:
+		block_definition = BlocksCatalog.get_property_getter_block_definition(variable)
 	else:
-		block_definition = (
-			BlockDefinition
-			. new(
-				&"%s_get_%s" % [object_name, property_name],
-				object_name,
-				"The %s property" % property_name,
-				"Variables",
-				Types.BlockType.VALUE,
-				property_type,
-				"%s" % property_name.capitalize().to_lower(),
-				"%s" % property_name,
-			)
-		)
+		block_definition = BlocksCatalog.get_property_setter_block_definition(variable)
+		block_definition.defaults = {"value": property_value}
 
 	var block = _context.block_script.instantiate_block(block_definition)
 	add_block(block, at_position)