endlessm/godot-block-coding

commit 89beea94eefa26d47e5059f73a4804f771323fae

BlockCanvas: Implement drag & drop the node's property from Inspector
Dragging the node's property from Inspector shows the object's type as
"obj_property". So, allow the "obj_property" type in _can_drop_data()
and handle it in _drop_data(). Then, implement getting the property's
value as a Variable block with _drop_obj_property() in detail.

https://phabricator.endlessm.com/T35649

addons/block_code/ui/block_canvas/block_canvas.gd
@@ -4,9 +4,11 @@ extends MarginContainer
 const ASTList = preload("res://addons/block_code/code_generation/ast_list.gd")
 const BlockAST = preload("res://addons/block_code/code_generation/block_ast.gd")
 const BlockCodePlugin = preload("res://addons/block_code/block_code_plugin.gd")
+const BlockDefinition = preload("res://addons/block_code/code_generation/block_definition.gd")
 const BlockTreeUtil = preload("res://addons/block_code/ui/block_tree_util.gd")
 const DragManager = preload("res://addons/block_code/drag_manager/drag_manager.gd")
 const ScriptGenerator = preload("res://addons/block_code/code_generation/script_generator.gd")
+const Types = preload("res://addons/block_code/types/types.gd")
 const Util = preload("res://addons/block_code/ui/util.gd")
 
 const EXTEND_MARGIN: float = 800
@@ -75,6 +77,10 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	if typeof(data) != TYPE_DICTIONARY:
 		return false
 
+	# Allow dropping property block
+	if data.get("type", "") == "obj_property":
+		return true
+
 	var nodes: Array = data.get("nodes", [])
 	if nodes.size() != 1:
 		return false
@@ -94,6 +100,8 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 func _drop_data(at_position: Vector2, data: Variant) -> void:
 	if data["type"] == "nodes":
 		_drop_node(at_position, data)
+	elif data["type"] == "obj_property":
+		_drop_obj_property(at_position, data)
 
 
 func _drop_node(at_position: Vector2, data: Variant) -> void:
@@ -109,6 +117,30 @@ func _drop_node(at_position: Vector2, data: Variant) -> void:
 
 	var block = _context.block_script.instantiate_block_by_name(&"get_node")
 	block.set_parameter_values_on_ready({"path": node_path})
+	add_block(block, at_position)
+	reconnect_block.emit(block)
+
+
+func _drop_obj_property(at_position: Vector2, data: Variant) -> void:
+	var object_name = str(data["object"]).get_slice(":", 0)
+	var property_name = data["property"]
+	var property_value = data["value"]
+
+	# Prepare a block to get the property's value
+	var block_definition = (
+		BlockDefinition
+		. new(
+			&"%s_get_%s" % [object_name, property_name],
+			object_name,
+			"The %s property" % property_name,
+			"Variables",
+			Types.BlockType.VALUE,
+			typeof(property_value),
+			"%s" % property_name.capitalize().to_lower(),
+			"%s" % property_name,
+		)
+	)
+	var block = _context.block_script.instantiate_block(block_definition)
 	add_block(block, at_position)
 	reconnect_block.emit(block)
 

commit 5c3358d3fb6ca4c2f5d5072f934cd56f7786352f

BlockCanvas: Only can drop properties of the BlockCode's parent node
Godot allows drag any thing. However, if it is a property, block code
script only manipulates the properties of BlockCode node's parent node.
So, when drag a property, only allow drop the parent node's property.

Fixes: 89beea94eefa ("BlockCanvas: Implement drag & drop the node's property from Inspector")
https://phabricator.endlessm.com/T35649

addons/block_code/ui/block_canvas/block_canvas.gd
@@ -81,8 +81,10 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	if typeof(data) != TYPE_DICTIONARY:
 		return false
 
-	# Allow dropping property block
+	# Allow dropping property block of the block code node's parent node
 	if data.get("type", "") == "obj_property":
+		if data["object"] != _context.parent_node:
+			return false
 		return true
 
 	var nodes: Array = data.get("nodes", [])